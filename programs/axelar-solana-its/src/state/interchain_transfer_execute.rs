6f217cbdce54812308f5d6f94f7d773530971dcf
Guilherme Felipe da Silva <gfsilva.eng@gmail.com>
2025-09-17 16:04:54 +0200

    fix(its): remove unused imports

--- programs/axelar-solana-its/src/state/interchain_transfer_execute.rs
+++ programs/axelar-solana-its/src/state/interchain_transfer_execute.rs
@@ -1,14 +1,9 @@
 //! State module contains data structures that keep state within the ITS
 //! program.
 
-use core::any::type_name;
-use core::mem::size_of;
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use program_utils::pda::BorshPda;
-use solana_program::msg;
-use solana_program::program_error::ProgramError;
-use solana_program::program_pack::{Pack, Sealed};
 
 /// Signed PDA to prove that ITS called an executable indeed. Only stores it's bump.
 #[derive(Debug, Eq, PartialEq, Clone, BorshSerialize, BorshDeserialize)]

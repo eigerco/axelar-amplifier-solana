81a4fe0401462f6c0d21a0b83237cdf8bb3223d4
Nikola Ristić <ristic@risticnikola.com>
2025-09-22 18:30:31 +0200

    ci: fix clippy issues

--- helpers/anchor-discriminators-macros/tests/instructions.rs
+++ helpers/anchor-discriminators-macros/tests/instructions.rs
@@ -24,7 +24,10 @@ pub enum GasServiceInstruction {
 #[test]
 fn test_discriminator() {
     let init = GasServiceInstruction::Initialize;
-    println!("Init disc: {:?}", init.discriminator());
+    assert_eq!(
+        &[175, 175, 109, 31, 13, 152, 155, 237],
+        init.discriminator()
+    );
 }
 
 #[test]
@@ -48,8 +51,10 @@ fn test_serialization() {
         refund_address: Pubkey::new_unique(),
     };
     let add_gas_data = borsh::to_vec(&add_gas).unwrap();
-    assert_eq!(&add_gas_data[..8], add_gas.discriminator());
+    #[allow(clippy::indexing_slicing)]
+    let add_gas_data = &add_gas_data[..8];
+    assert_eq!(add_gas_data, add_gas.discriminator());
 
-    let add_gas_deser = GasServiceInstruction::try_from_slice(&add_gas_data).unwrap();
+    let add_gas_deser = GasServiceInstruction::try_from_slice(add_gas_data).unwrap();
     assert_eq!(add_gas, add_gas_deser);
 }
